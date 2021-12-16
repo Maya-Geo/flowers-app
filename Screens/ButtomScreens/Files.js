1ddd53bb42fafc77224610c3d479be6e8a47628d
Maya Hagan <miyaclessa@gmail.com>
2021-12-17 00:32:01 +1300

    added FilesGif

---
+++ Screens/ButtomScreens/Files.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+
+const Files = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.pageHeading}>Files Page</Text>
+
+      <View>
+        <Image style={styles.gif} source={require("../../assets/files.gif")} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Files;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  pageHeading: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+
+  gif: {
+    width: 400,
+    height: 400,
+  },
+});
