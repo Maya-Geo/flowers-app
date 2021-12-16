cb9beb525055c314f6d27b88769a11d9026ea065
Maya Hagan <miyaclessa@gmail.com>
2021-12-17 00:31:27 +1300

    added BottomNavigator

---
+++ Screens/ButtomScreens/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import BottomNavigator from "../../NavigatorRoute/BottomNavigator";
+const Home = () => {
+  return <BottomNavigator />;
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
